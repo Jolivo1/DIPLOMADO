e05d40923a6ce6803b4d229cba7ccf4f3bb27f49
Joaquin Olivo <joaquin.olivo@hotmail.com>
2022-09-07 22:18:24 -0500

    adding deploy

---
+++ crud/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzKsSxTygHV0rIZryI2gB4ohRQmq2DvIU",
+  authDomain: "crudfire-1c60c.firebaseapp.com",
+  projectId: "crudfire-1c60c",
+  storageBucket: "crudfire-1c60c.appspot.com",
+  messagingSenderId: "935539067319",
+  appId: "1:935539067319:web:6ea573e172e5e22e488032"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export {firebase};
